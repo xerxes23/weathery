3b3217cf8644ea24610b4a5e2e1cb20f9c1348fc
Joseph Mendoza <josephmendoza23@gmail.com>
2018-07-25 13:36:05 -0400

    Added Yargs to the application

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 const request = require("request");
+const yargs = require("yargs");
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: "address",
+      describe: "Address to fetch wheater for",
+      string: true
+    }
+  })
+  .help()
+  .alias("help", "h").argv;
+
+const encodedAddress = encodeURIComponent(argv.a);
 
 request(
   {
-    url:
-      "https://maps.googleapis.com/maps/api/geocode/json?address=95%20seaman%20avenue%20new%20york",
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyA3x4f15TetG8L97tXIGPRhyFEkSiZL0HM`,
     json: true
   },
   (error, response, body) => {
-    console.log(body);
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(
+      `Latitude: ${body.results[0].geometry.location.lat}, Longitude: ${
+        body.results[0].geometry.location.lng
+      }`
+    );
   }
 );
