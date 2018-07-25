9e3113d666d7b8f6cbc993acddc973b28d6694e6
Joseph Mendoza <josephmendoza23@gmail.com>
2018-07-25 13:57:08 -0400

    Implemented error handling for finding address

--- app.js
+++ app.js
@@ -21,11 +21,17 @@ request(
     json: true
   },
   (error, response, body) => {
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(
-      `Latitude: ${body.results[0].geometry.location.lat}, Longitude: ${
-        body.results[0].geometry.location.lng
-      }`
-    );
+    if (error) {
+      console.log("Unable to connect to Google servers.");
+    } else if (body.status === "ZERO_RESULTS") {
+      console.log("Unable to find that address.");
+    } else if (body.status === "OK") {
+      console.log(`Address: ${body.results[0].formatted_address}`);
+      console.log(
+        `Latitude: ${body.results[0].geometry.location.lat}, Longitude: ${
+          body.results[0].geometry.location.lng
+        }`
+      );
+    }
   }
 );
