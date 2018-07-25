05b98dadaa82536fa0d960ac7d973d56509e22c7
Joseph Mendoza <josephmendoza23@gmail.com>
2018-07-25 14:12:50 -0400

    Refactored for modularity

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const request = require("request");
 const yargs = require("yargs");
+const geocode = require("./geocode/geocode");
 
 const argv = yargs
   .options({
@@ -13,25 +13,4 @@ const argv = yargs
   .help()
   .alias("help", "h").argv;
 
-const encodedAddress = encodeURIComponent(argv.a);
-
-request(
-  {
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyA3x4f15TetG8L97tXIGPRhyFEkSiZL0HM`,
-    json: true
-  },
-  (error, response, body) => {
-    if (error) {
-      console.log("Unable to connect to Google servers.");
-    } else if (body.status === "ZERO_RESULTS") {
-      console.log("Unable to find that address.");
-    } else if (body.status === "OK") {
-      console.log(`Address: ${body.results[0].formatted_address}`);
-      console.log(
-        `Latitude: ${body.results[0].geometry.location.lat}, Longitude: ${
-          body.results[0].geometry.location.lng
-        }`
-      );
-    }
-  }
-);
+geocode.geocodeAddress(argv.a);
